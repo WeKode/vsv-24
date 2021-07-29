b98fdabcb3e3cce0aaba95595705f28a5f1bb478
Abd Elbasset <karaliabdelbasset@outlook.com>
2021-07-29 18:01:20 +0100

    Create Admin Model

---
+++ app/Models/Admin.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
+
+class Admin extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'pic',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $appends = [
+        'pic_url',
+    ];
+
+    public function getPicUrlAttribute()
+    {
+        if (Str::contains($this->pic,'http'))
+        {
+            return $this->pic;
+        }
+        return $this->pic ? asset('storage/'.$this->pic) : asset('');
+    }
+}
