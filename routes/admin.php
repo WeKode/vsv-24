0f86f4eecfa49e9cb48c2e9a0ca73fdb3d4523b3
zakariaMogh <zakaria.dehaba@gmail.com>
2021-07-30 20:27:16 +0100

    routes fix

--- routes/admin.php
+++ routes/admin.php
@@ -20,9 +20,11 @@ Route::middleware('auth:admin')->group(function (){
     Route::resource('admins',\App\Http\Controllers\Admin\AdminController::class);
     Route::resource('brands',\App\Http\Controllers\Admin\BrandController::class);
     Route::resource('attributes',\App\Http\Controllers\Admin\AttributeController::class);
-    Route::resource('products',\App\Http\Controllers\Admin\ProductController::class);
 
     Route::post('products/image/upload',[App\Http\Controllers\Admin\ImageController::class,'store'])->name('products.images.upload');
     Route::get('products/{id}/delete',[App\Http\Controllers\Admin\ImageController::class,'destroy'])->name('products.images.delete');
+    Route::resource('products',\App\Http\Controllers\Admin\ProductController::class);
+
+
 
 });
