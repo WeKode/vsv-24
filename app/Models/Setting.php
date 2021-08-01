a40db98c848aa6ed203274da6036c31c271a3bf6
Abd Elbasset <karaliabdelbasset@outlook.com>
2021-08-01 16:44:06 +0100

    Create Setting model,migration and SettingServiceProvider

---
+++ app/Models/Setting.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Config;
+
+class Setting extends Model
+{
+    use HasFactory;
+
+    /**
+     * @var string[]
+     */
+    protected $fillable = [
+        'key','value'
+    ];
+
+    /**
+     * @param $key
+     * @return string|null
+     */
+    public static function get($key) :?string
+    {
+        $entry = self::where('key', $key)->first();
+        return $entry->value ?? null;
+    }
+
+    /**
+     * @param $key
+     * @param null $value
+     * @return bool
+     */
+    public static function set($key, $value = null): bool
+    {
+        $entry = self::where('key', $key)->firstOrFail();
+        $entry->update([
+            'value' => $value
+        ]);
+        Config::set('key', $value);
+        return Config::get($key) === $value;
+    }
+}
