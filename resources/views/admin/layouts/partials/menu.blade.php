6d7cafad3322561c40f0bf06523eb1a839c0d000
Abd Elbasset <karaliabdelbasset@outlook.com>
2021-08-04 18:17:40 +0100

    menu ma3labalish bih

--- resources/views/admin/layouts/partials/menu.blade.php
+++ resources/views/admin/layouts/partials/menu.blade.php
@@ -2,7 +2,7 @@
     <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
         <!-- Add icons to the links using the .nav-icon class
              with font-awesome or any other icon font library -->
-        <li class="nav-item ">
+        <li class="nav-item">
             <a href="{{route('admin.dashboard')}}" data-link="/admin/dashboard"
                class="nav-link {{request()->routeIs('admin.dashboard*') ? 'active' : ''}}">
                 <i class="nav-icon fas fa-tachometer-alt"></i>
