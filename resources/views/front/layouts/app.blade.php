0aedb99970b912004fe8b82c62123cf484819898
Zertal Raouf <Z.raouf97@gmail.com>
2021-08-06 09:18:05 +0100

    Update cart icon clickable

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -28,9 +28,11 @@
 
 @yield('content')
 
-<div class="position-fixed bg-warning text-black shadow-sm" style="bottom: 50px; right: 50px; z-index: 999;width: 60px;height: 60px;display: flex;justify-content: center;align-items: center;border-radius: 50%">
-    <i class="fas fa-shopping-cart"></i><span class="badge bg-danger small position-absolute" style="top: -5px;right: -5px;">0</span>
-</div>
+<a href="" class="text-decoration-none">
+    <div class="position-fixed bg-warning text-black shadow-sm" style="bottom: 50px; right: 50px; z-index: 999;width: 60px;height: 60px;display: flex;justify-content: center;align-items: center;border-radius: 50%">
+        <i class="fas fa-shopping-cart text-black"></i><span class="badge bg-danger small position-absolute text-light" style="top: -5px;right: -5px;">0</span>
+    </div>
+</a>
 
 @include('front.layouts.partials.footer')
 
